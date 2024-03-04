ca875ea3ab116543b4042d946c3fdfe28c704f73
Joel Smith <smithjoelt@gmail.com>
2024-03-04 08:58:01 -0800

    Added loop for multiple guesses. Also added error handling for when user enters non-numerical characters.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -10,21 +10,30 @@ fn main() {
 
    println!("The secret number is: {secret_number}");
 
-   println!("please input your guess.");
+   loop {
+      println!("please input your guess.");
 
-   let mut guess = String::new();
+      let mut guess = String::new();
 
-   io::stdin()
-      .read_line(&mut guess)
-      .expect("Failed to read line");
+      io::stdin()
+         .read_line(&mut guess)
+         .expect("Failed to read line");
 
-   let guess: u32 = guess.trim().parse().expect("Please type a number!");
+      let guess: u32 = match guess.trim().parse() {
+         Ok(num) => num,
+         Err(_) => continue,
+      };
 
-   println!("You guessed: {guess}");
 
-   match guess.cmp(&secret_number) {
-      Ordering::Less => println!("Too small!"),
-      Ordering::Greater => println!("Too big!"),
-      Ordering::Equal => println!("You win!"),
+      println!("You guessed: {guess}");
+
+      match guess.cmp(&secret_number) {
+         Ordering::Less => println!("Too small!"),
+         Ordering::Greater => println!("Too big!"),
+         Ordering::Equal => {
+            println!("You win!");
+            break;
+         }
+      }
    }
 }
